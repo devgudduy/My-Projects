bd4f35e1dc83034cab3ad8bd1f4628266134414c
Guddu Kumar <guddu12022003@gmail.com>
2025-03-20 21:42:43 +0530

    Update script.js

---
+++ Captcha-Generator/script.js
@@ -0,0 +1,10 @@
+const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const captcha = document.querySelector(".captcha");
+const input = document.querySelector(".input");
+const refresh = document.querySelector(".refresh");
+const submit = document.querySelector(".submit");
+
+submit.addEventListener('click', ()=>{
+  const inpval = input.value;
+  // const 
+})
