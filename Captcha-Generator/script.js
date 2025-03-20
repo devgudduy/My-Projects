c6df6219989ba1cf428e73dadbc5737b33bd915b
Guddu Kumar <guddu12022003@gmail.com>
2025-03-20 23:14:44 +0530

    Update script.js

--- Captcha-Generator/script.js
+++ Captcha-Generator/script.js
@@ -1,10 +1,28 @@
 const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 const captcha = document.querySelector(".captcha");
-const input = document.querySelector(".input");
-const refresh = document.querySelector(".refresh");
-const submit = document.querySelector(".submit");
+const input = document.querySelector("#captcha-input");
+const refresh = document.querySelector("#refresh");
+const submit = document.querySelector("#submit");
 
-submit.addEventListener('click', ()=>{
+submit.addEventListener("click", () => {
   const inpval = input.value;
-  // const 
-})
+  const captchaval = captcha.textContent;
+  if (inpval == captchaval) {
+    alert("Captcha is Matched");
+  } else {
+    alert("Captcha is not Matched");
+  }
+});
+
+refresh.addEventListener("click", () => {
+  captcha.textContent = generateCaptcha();
+});
+
+function generateCaptcha() {
+  let captcha_gen = "";
+  for (let i = 0; i < 6; i++) {
+    captcha_gen += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return (captcha.textContent = captcha_gen);
+}
+console.log(generateCaptcha());
