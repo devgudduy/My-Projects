cdfe36606b2fc578c8b0e5b2ee5bb53eade8cbc5
Guddu Kumar <guddu12022003@gmail.com>
2025-03-31 23:42:48 +0530

    Update script.js

--- Weight-Converter/script.js
+++ Weight-Converter/script.js
@@ -1,6 +1,5 @@
 const input = document.querySelector("#input");
 const output = document.querySelector("#output");
-
 input.addEventListener("input", () => {
   let val = input.value;
   let out = val * 2.2;
