11747f5e4a999ab3c2b8d01ee3ccc827572deef5
Guddu Kumar <guddu12022003@gmail.com>
2025-04-14 23:33:16 +0530

    Update script.js

--- Captcha-Generator/script.js
+++ Captcha-Generator/script.js
@@ -13,9 +13,11 @@ submit.addEventListener("click", () => {
     alert("Captcha is not Matched");
   }
 });
+
 refresh.addEventListener("click", () => {
   captcha.textContent = generateCaptcha();
 });
+
 function generateCaptcha() {
   let captcha_gen = "";
   for (let i = 0; i < 6; i++) {
